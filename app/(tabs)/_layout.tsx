a25621bd992f1b3b03d20563b3971f9dd153d1a9
cbeAbishek <agsabhishek1907@gmail.com>
2025-04-26 16:47:49 +0530

    local image storage implemented

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,10 +34,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Camera Page',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
         }}
       />
     </Tabs>
