5def19a4f2c0983dcb1dab4dac5497b61572225f
cbeAbishek <agsabhishek1907@gmail.com>
2025-04-26 16:47:49 +0530

    local image storage implemented

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,12 +13,14 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'camera.fill': 'photo-camera', // <-- Added this line
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
     React.ComponentProps<typeof MaterialIcons>['name']
   >
 >;
+
 
 export type IconSymbolName = keyof typeof MAPPING;
 
