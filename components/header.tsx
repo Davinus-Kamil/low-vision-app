df8ef407527ca2abb4aafc3d4f969f905122c418
Darsh200618 <darshangopal2006@gmail.com>
2025-05-08 14:54:56 +0530

    header created

--- components/header.tsx
+++ components/header.tsx
@@ -1,44 +1,49 @@
-import React from 'react';
-import { View, Text,StyleSheet } from 'react-native'
-import { useRouter } from 'expo-router';
-import { HelloWave } from './HelloWave';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons"; 
 
 const Header: React.FC = () => {
   const router = useRouter();
 
   return (
     <View style={styles.header}>
+      <TouchableOpacity onPress={() => console.log("Exit pressed")}>
+        <Ionicons name="close" size={24} color="white" />
+      </TouchableOpacity>
       <Text style={styles.title}>Low-Vision </Text>
-      <HelloWave />
+      <TouchableOpacity onPress={() => console.log("Menu pressed")}>
+        <Ionicons name="menu" size={28} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#DF2935',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#DF2935",
     paddingHorizontal: 20,
     paddingVertical: 10,
     height: 60,
   },
   logo: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   title: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     flex: 1,
-    textAlign: 'center',
+    textAlign: "center",
   },
   iconContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   icon: {
     marginLeft: 15,
